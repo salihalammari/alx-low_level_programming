c852a44536ce9933b881cf13f6e1b949e4875fa1
salihalammari <slammari@student.1337.ma>
2023-03-22 00:55:29 +0000

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
 
 
 void print_alphabet(void)
@@ -15,6 +15,6 @@ void print_alphabet(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
-_putchar('\n');
+	_putchar('\n');
 }
 
